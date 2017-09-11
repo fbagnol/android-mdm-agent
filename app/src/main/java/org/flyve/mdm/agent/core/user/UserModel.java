924094cf8076eee41e4e5833700881e403cd7ca7
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(getPhone): add the return of the tag"
    This reverts commit f1092d50e4c23de602bfb0ff78ae6b725627c819.

--- app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/user/UserModel.java
@@ -107,10 +107,6 @@ public class UserModel {
         this.language = language;
     }
 
-    /**
-     * Get the phone of the user
-     * @return the phone
-     */
     public String getPhone() {
         return phone;
     }
