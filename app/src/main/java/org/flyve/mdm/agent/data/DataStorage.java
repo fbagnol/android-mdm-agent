40d2a32bf6f54c66e177c757ffa7c859a8fac097
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(setEntitiesId): add the param of the tag"
    This reverts commit 529865e887cdf7e8b54cec2f6405802d7477a4b8.

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -335,10 +335,6 @@ public class DataStorage {
 		return getData("entities_id");
 	}
 
-	/**
-	 * Set the ID of the entities
-	 * @param string the entities ID
-	 */
 	public void setEntitiesId(String entitiesId) {
 		setData("entities_id", entitiesId);
 	}
