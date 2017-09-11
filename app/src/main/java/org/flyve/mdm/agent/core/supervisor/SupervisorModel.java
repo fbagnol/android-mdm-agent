5fe9178af0b32ddfb472986d332adaaffa3d9c3c
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(getWebsite): add the return of the tag"
    This reverts commit 156fdf8ab16188ae2d5d8bf61020d30679f4c9de.

--- app/src/main/java/org/flyve/mdm/agent/core/supervisor/SupervisorModel.java
+++ app/src/main/java/org/flyve/mdm/agent/core/supervisor/SupervisorModel.java
@@ -93,10 +93,6 @@ public class SupervisorModel {
         this.phone = phone;
     }
 
-    /**
-     * Get the website of the Supervisor Model
-     * @return string the website
-     */
     public String getWebsite() {
         return website;
     }
