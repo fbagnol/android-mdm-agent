6de02e690cfdac1eea22c7be1332a3380f811830
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(getName): add the return of the tag"
    This reverts commit a0dc70920d507b0bc91debe36263bddf2074bbde.

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -295,10 +295,6 @@ public class DataStorage {
 		setData("certificate", certificate);
 	}
 
-	/**
-	 * Get the name
-	 * @return string the name
-	 */
 	public String getName() {
 		return getData("name");
 	}
