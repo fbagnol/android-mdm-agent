73a20e5fd14b09712793a7692c5d84cfe06b0ce3
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(getTopic): add the return of the tag"
    This reverts commit 58a4a95d42b412dd079ba13f1b4df32d81dc6932.

--- app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
+++ app/src/main/java/org/flyve/mdm/agent/data/DataStorage.java
@@ -231,10 +231,6 @@ public class DataStorage {
 		setData("tls", tls);
 	}
 
-	/**
-	 * Get the topic
-	 * @return string the topic
-	 */
 	public String getTopic() {
 		return getData("topic");
 	}
