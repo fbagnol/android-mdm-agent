ce0578adc3a2ff37b1f9bae50f39d90a1056f8f5
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(slideDots): add the param of the tag"
    This reverts commit 00352a8bcc171a1b2ce304324388e9e0f90f10d6.

--- app/src/main/java/org/flyve/mdm/agent/ui/FragmentSlideWalkthrough.java
+++ app/src/main/java/org/flyve/mdm/agent/ui/FragmentSlideWalkthrough.java
@@ -78,10 +78,6 @@ public class FragmentSlideWalkthrough extends Fragment {
         return v;
     }
 
-    /**
-     * Shows the slide dots of the screen
-     * @param ViewGroup the view
-     */
     private void slideDots(ViewGroup v) {
 
         LinearLayout ln = (LinearLayout) v.findViewById(R.id.lnSliders);
