d2ccedb38116548eafd66bfe2275b6fb62cbc69c
Rafa Hernández <rhernandez@teclib.com>
2017-09-11 18:40:56 -0400

    Revert "docs(getPicturesDir): add the return and throws tags"
    This reverts commit c475fe70fd22b05afb2d40bbd6cfcffa8daf9dcd.

--- app/src/main/java/org/flyve/mdm/agent/utils/FilesHelper.java
+++ app/src/main/java/org/flyve/mdm/agent/utils/FilesHelper.java
@@ -74,11 +74,6 @@ public class FilesHelper {
         return System.getenv("EXTERNAL_STORAGE") + "/.fdroid/";
     }
 
-    /**
-     * Get the directory of the pictures
-     * @return string the pictures directory
-     * @throws Exception
-     */
     private static String getPicturesDir() throws Exception {
         FlyveLog.d(System.getenv("EXTERNAL_STORAGE") + "/" + Environment.DIRECTORY_DCIM);
         return System.getenv("EXTERNAL_STORAGE") + "/" + Environment.DIRECTORY_DCIM;
